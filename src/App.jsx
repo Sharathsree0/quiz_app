b9f040d071b8cf59653aee406b37cb55abca2872
sharath <sharathsree916@gmail.com>
2025-10-23 10:37:32 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import AdminDashboard from './components/Admin/AdminDashboard'
+import Homepage from './pages/Homepage'
+import { Route, Routes } from 'react-router-dom'
+import Quizpage from './pages/Quizpage'
 function App() {
   return (
-    <AdminDashboard></AdminDashboard> 
+   <>
+    <Routes>
+      <Route path='/' element={<Homepage/>} />
+      <Route path='/quiz' element={<Quizpage/>} />
+
+    </Routes>
+   </>
   )
 }
 
---
+++ src/pages/Quizpage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Quizpage() {
+  return (
+    <div>Quiz start here..</div>
+  )
+}
+
+export default Quizpage
