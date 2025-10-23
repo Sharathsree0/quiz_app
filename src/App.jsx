e1d08427e59509dd6151453bdc11e50b30d34c31
sharath <sharathsree916@gmail.com>
2025-10-23 09:46:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AdminDashboard from './components/Admin/AdminDashboard'
+function App() {
+  return (
+    <AdminDashboard></AdminDashboard> 
+  )
+}
+
+export default App
